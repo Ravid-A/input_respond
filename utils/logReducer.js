a5d12e6a96cdb10a608490a9edcd506f6575e77f
Ravid-A <ravid2903@gmail.com>
2023-11-28 16:43:50 +0200

    removed stupid break

--- utils/logReducer.js
+++ utils/logReducer.js
@@ -13,7 +13,6 @@ export default function logReducer(log, action) {
                     rage: action.rage,
                 },
             ];
-            break;
         } 
         case "reset":
         {
