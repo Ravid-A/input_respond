0be4e7210495e0a3be1448c8556a1bdfef24578a
Ravid-A <ravid2903@gmail.com>
2023-11-29 01:26:53 +0200

    sperated each action type to a function

--- utils/battleReducerContext.js
+++ utils/battleReducerContext.js
@@ -27,7 +27,76 @@ export function useLogDispatch() {
     return useContext(battleDispatchContext);
 }
 
-function logReducer(log, action) {
+function logReducer(log, action) 
+{
+    switch (action.type) {
+        case "select":
+        {
+            return selectPokemon(log, action);
+        }
+        case "ready":
+        {
+            return readyForBattle(log);
+        }
+        case "start":
+        {
+            return startBattle(log);
+        }
+        case "attack":
+        {
+            return attackPokemon(log, action);
+        }
+        case "end":
+        {
+            return endBattle(log);
+        }
+        case "reset":
+        {
+           return resetBattle(log);
+        }
+    } 
+
+    throw Error('Unknown action: ' + action.type);
+}
+
+function selectPokemon(log, action) 
+{
+    const selectedPokemons = useSelectedPokemons();
+    const setSelectedPokemons = useSetSelectedPokemons();
+
+    const { pokemon } = action;
+
+    if(selectedPokemons.length < 2) {
+        setSelectedPokemons([...selectedPokemons, pokemon]);
+    }
+
+    return [...log, `• ${pokemon.name} has joined the battle`];
+}
+
+function readyForBattle(log) 
+{
+    const setStatus = useSetStatus();
+
+    setStatus("ready");
+    return [...log, "• All pokemons are ready for the battle"];
+}
+
+function startBattle(log) 
+{
+    const setStatus = useSetStatus();
+
+    setStatus("start");
+    return [...log, "• Battle has started"];
+}
+
+function attackPokemon(log, action) 
+{
+    function get_damage(damage) 
+    {
+        const {low, high} = damage;
+    
+        return Math.floor(Math.random() * (high - low + 1) + low);
+    }
 
     const selectedPokemons = useSelectedPokemons();
     const setSelectedPokemons = useSetSelectedPokemons();
@@ -37,98 +106,59 @@ function logReducer(log, action) {
 
     const setStatus = useSetStatus();
 
-    switch (action.type) {
-        case "select":
-        {
-            const { pokemon } = action;
+    const { pokemon, rage } = action;
 
-            if(selectedPokemons.length < 2) {
-                setSelectedPokemons([...selectedPokemons, pokemon]);
-            }
+    const damage = get_damage(pokemon.attack.damage) * (rage ? 2 : 1);
+    const dodge = Math.random() < (rage ? 0.25 : 0.5);
+    
+    const target_index = (current_turn + 1) % 2;
+    const target = selectedPokemons[target_index];
 
-            return [...log, `${pokemon.name} has joined the battle`];
-        }
-        case "ready":
-        {
-            setStatus("ready");
-            return [...log, "All pokemons are ready for the battle"];
-        }
-        case "start":
-        {
-            setStatus("start");
-            return [...log, "Battle has started"];
-        }
-        case "attack":
-        {
-            const { pokemon, rage } = action;
-
-            const damage = get_damage(pokemon.attack.damage) * (rage ? 2 : 1);
-            const dodge = Math.random() < (rage ? 0.25 : 0.5);
-            
-            const target_index = (current_turn + 1) % 2;
-            const target = selectedPokemons[target_index];
-
-            if(!dodge)
-            {
-                const new_hp = Math.max(target.hp - damage, 0);
-
-                selectedPokemons[target_index] = {
-                    ...target,
-                    hp: new_hp,
-                };
-
-                if(new_hp === 0)
-                {
-                    setStatus("dead");
-                }
-
-                setSelectedPokemons([...selectedPokemons]);
-            }
-
-            setCurrentTurn(target_index);
-
-            return [...log, get_log_text(pokemon, target, damage, dodge, rage)];
-        }
-        case "end":
-        {
-            const winner = selectedPokemons[0].hp > 0 ? selectedPokemons[0] : selectedPokemons[1];
-
-            setStatus("end");
-            return [...log, `${winner.name} won the battle`];
-        }
-        case "reset":
-        {
-            setSelectedPokemons([]);
-            setCurrentTurn(0);
-            setStatus("prepare");
-
-            return [];
-        }  
-    } 
-}
-
-function get_damage(damage) 
-{
-    const {low, high} = damage;
-
-    return Math.floor(Math.random() * (high - low + 1) + low);
-}
-
-function get_log_text(pokemon, target, damage, dodge, rage)
-{
-    let logText = `${pokemon.name} attacked ${target.name} using ${pokemon.attack.name} ability`;
-
-    if(rage)
+    if(!dodge)
     {
-        logText += ` with Rage`;
+        const new_hp = Math.max(target.hp - damage, 0);
+
+        selectedPokemons[target_index] = {
+            ...target,
+            hp: new_hp,
+        };
+
+        if(new_hp === 0)
+        {
+            setStatus("dead");
+        }
+
+        setSelectedPokemons([...selectedPokemons]);
     }
 
-    if(dodge)
-    {
-        logText += ` but ${target.name} dodged`;
-    } else {
-        logText += ` and made ${damage} damage`;
-    }
+    setCurrentTurn(target_index);
 
-    return logText;
+    let logText = `• ${pokemon.name} attacked ${target.name} using ${pokemon.attack.name}${rage ? ' with Rage' : ''}.${dodge ? ` ${target.name} dodged the attack.` : ` The attack dealt ${damage} damage.`}`;
+
+    return [...log, logText];
+}
+
+function endBattle(log) 
+{
+    const selectedPokemons = useSelectedPokemons();
+    const setStatus = useSetStatus();
+
+    const winner = selectedPokemons[0].hp > 0 ? selectedPokemons[0] : selectedPokemons[1];
+
+    setStatus("end");
+    return [...log, `• ${winner.name} won the battle`];
+}
+
+function resetBattle(log) 
+{
+    const setSelectedPokemons = useSetSelectedPokemons();
+    const setCurrentTurn = useSetCurrentTurn();
+    const setStatus = useSetStatus();
+
+
+    setSelectedPokemons([]);
+    setCurrentTurn(0);
+    setStatus("prepare");
+
+    return [];
 }
